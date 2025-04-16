cc9b30223945e772c58fb0add6fe1d2f0b5d7133
Nikita Shakhov <supersoter@yandex.ru>
2025-04-16 08:22:39 +0300

    Update main.py add root view

--- movie-catalog/main.py
+++ movie-catalog/main.py
@@ -1,6 +1,22 @@
 from fastapi import FastAPI
+from starlette.requests import Request
 
 app = FastAPI(
     title="Movie Catalog",
-    description="An application for working with movie catalogs"
+    description="An application for working with movie catalogs",
 )
+
+
+@app.get("/")
+def read_root(
+    request: Request,
+    name: str = "World",
+):
+    docs_url = request.url.replace(
+        path="/docs",
+        query="",
+    )
+    return {
+        "message": f"Hello {name}!",
+        "docs_url": str(docs_url),
+    }
